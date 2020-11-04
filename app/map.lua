1399a573f67237f11e4466ef074d1e1304b3a853
grinshpon <grinshpondaniel@protonmail.com>
2020-11-04 13:48:27 -0500

    grid coords to cartesian coords

--- app/map.lua
+++ app/map.lua
@@ -7,7 +7,20 @@ local Map = {Level = {}, }
 
 
 
+
+
+
+
 local Level = Map.Level
 
-local map
+local map = {}
+
+function map.grid2cart(l, x, y)
+
+   local theta = math.pi / math.pow(2, x)
+   theta = theta * (y + 0.5)
+   local r = l.width * (x + 0.5)
+   return r * math.cos(theta), r * math.sin(theta)
+end
+
 return map
